22d078531548d2ccd07548a15044ea693a94cef2
KeatinWerleman <keatin8756@gmail.com>
2024-09-11 18:31:53 -0500

    9/11

---
+++ FA 2024 Ball Puzzle Minigame/Assets/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float ballSpeed;
+    public Vector3 ballDirection;
+    public string winTag;
+    public string reflectTag;
+
+
+    
+
+   
+}
--- FA 2024 Ball Puzzle Minigame/Assets/Scripts/BallLauncher.cs
+++ FA 2024 Ball Puzzle Minigame/Assets/Scripts/BallLauncher.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallLauncher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject ballPrefab;
+    public float launchSpeed;
+    public GameObject ballSpawnPoint;
+    bool canLaunchBall = true;
+    public int availibleBalls;
 
-    // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Space) && canLaunchBall == true)
+        {
+            LaunchBall();
+            
+        }
+    }
+    void LaunchBall()
+    {
         
+        GameObject newBall = Instantiate(ballPrefab, ballSpawnPoint.transform.position, ballSpawnPoint.transform.rotation);
+        newBall.GetComponent<Rigidbody2D>().AddForce(ballSpawnPoint.transform.right * launchSpeed);
+        availibleBalls -= 1;
+        Debug.Log("Ball Launched/n Balls Remaining: " + availibleBalls);
     }
 }
