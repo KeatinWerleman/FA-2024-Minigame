81cf7fae55312d6d3df6402823648bd5c881cfc5
KeatinWerleman <keatin8756@gmail.com>
2025-04-01 22:53:41 -0500

    debugged tracks, added medium multiswitch level

--- FA 2024 Ball Puzzle Minigame/Assets/Scripts/Track.cs
+++ FA 2024 Ball Puzzle Minigame/Assets/Scripts/Track.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Track : MonoBehaviour
@@ -10,43 +11,45 @@ public class Track : MonoBehaviour
     public GameObject objectOnTrack;
     public GameObject trackConnector;
     public bool isObjectThere = true;
+    public float xOffset;
+    public float yOffset;
     
     
     public void Start()
     {
-        objectOnTrack.transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), transform.position.z);
+        objectOnTrack.transform.position = new Vector3(Mathf.Round(transform.localPosition.x), Mathf.Round(transform.localPosition.y), transform.localPosition.z);
+        
 
     }
     public void Update()
     {
-        CheckIfObjectIsOnTrack();
+        yOffset = Mathf.Abs(objectOnTrack.transform.localPosition.y - trackConnector.transform.localPosition.y);
+        xOffset = Mathf.Abs(objectOnTrack.transform.localPosition.x - trackConnector.transform.localPosition.x);
         
-
-
+            CheckIfObjectIsOnTrack();
     }
-
-    
-   public void CheckIfObjectIsOnTrack()
+    public void CheckIfObjectIsOnTrack()
     {
         if (objectOnTrack != null)
         {
-            if (lowerEndpoint.localPosition.y == upperEndpoint.transform.localPosition.y)
+            if (lowerEndpoint.transform.localPosition.y == upperEndpoint.transform.localPosition.y)
             {
-                if (objectOnTrack.transform.localPosition.y != lowerEndpoint.transform.localPosition.y ||
-                    objectOnTrack.transform.localPosition.x < lowerEndpoint.transform.localPosition.x ||
-                    objectOnTrack.transform.localPosition.x > upperEndpoint.transform.localPosition.x)
+                if (yOffset > 0.05f ||
+                    objectOnTrack.transform.localPosition.x > upperEndpoint.localPosition.x + 0.05f||
+                    objectOnTrack.transform.localPosition.x < lowerEndpoint.localPosition.x - 0.05f)  
                 {
-                    objectOnTrack.transform.localPosition = new Vector3(Mathf.Round(upperEndpoint.localPosition.x + lowerEndpoint.localPosition.x) / 2, upperEndpoint.localPosition.y, 0f);
+
+                    objectOnTrack.transform.localPosition = new Vector3(Mathf.Round((upperEndpoint.localPosition.x + lowerEndpoint.localPosition.x) / 2), upperEndpoint.localPosition.y, 0f);
                 }
             }
 
-            else if (lowerEndpoint.localPosition.x == upperEndpoint.transform.localPosition.x)
+            else if (lowerEndpoint.transform.localPosition.x == upperEndpoint.transform.localPosition.x)
             {
-                if (objectOnTrack.transform.localPosition.x != lowerEndpoint.transform.localPosition.x ||
-                    objectOnTrack.transform.localPosition.y < lowerEndpoint.transform.localPosition.y ||
-                    objectOnTrack.transform.localPosition.y > upperEndpoint.transform.localPosition.y)
+                if (xOffset > 0.05f ||
+                    objectOnTrack.transform.localPosition.y > upperEndpoint.localPosition.y + 0.05f ||
+                    objectOnTrack.transform.localPosition.y < lowerEndpoint.localPosition.y - 0.05f)
                 {
-                    objectOnTrack.transform.localPosition = new Vector3(upperEndpoint.localPosition.x, (Mathf.Round(upperEndpoint.localPosition.y + lowerEndpoint.localPosition.y) / 2), 0f);
+                    objectOnTrack.transform.localPosition = new Vector3(upperEndpoint.localPosition.x, (Mathf.Round((upperEndpoint.localPosition.y + lowerEndpoint.localPosition.y) / 2)), 0f);
                 }
             }
         }
